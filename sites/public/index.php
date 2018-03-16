8f287415bc48ece8f28cfa561d94c56273cce142
Dilanga Algama <dalga082@uottawa.ca>
2018-03-16 17:03:04 -0400

    DILANGA INITIAL SETUP

--- sites/public/index.php
+++ sites/public/index.php
@@ -2,10 +2,11 @@
 require_once('Model.php');
 require_once('Connection.php');
 try {
+    var_dump(PDO::getAvailableDrivers());
     $conn = Connection::init()->getConnection();
     $statement = $conn->query('select * from customer');
     $r = $statement->fetchAll();
     var_dump($r);
 } catch (PDOException $e){
-    echo $e->getMessage();
+    echo $e;
 }
