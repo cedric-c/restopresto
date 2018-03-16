fbe5b5de7c35cd2ae58fd46c85d8afc9f56b4ddd
Cédric Clément <cclem054@uottawa.ca>
2018-03-15 23:35:21 -0400

    Introduced new Connection class.

---
+++ sites/public/Connection.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @author Cédric Clément <cclem054@uottawa.ca>
+ * @version 1.0
+ * @since 1.0
+ * (c) Copyright 2018 Cédric Clément.
+ * 
+ * DBC – Database Connection class used to communicated with a database.
+ */
+
+class Connection {
+    /**
+     * Connection object implementing singleton design patter.
+     * @var Connection
+     */
+    private static $connection;
+    
+    private function __construct(){}
+    
+    /**
+     * Returns a connection object. Makes use of the singleton design pattern in order to only have a single connection object for a user.
+     * @return Connection
+     */
+    public static function init(): Connection {
+        if (null === self::$connection){
+            self::$connection = new Connection();
+        }
+        return self::$connection;
+    }
+    
+    /**
+     * Returns a PhpDatabaseObject (PDO) which will be used to communicate with the database.
+     * @return PDO
+     */
+    public function getConnection(): PDO {
+        $settings = parse_ini_file('../db-config.ini', true);
+        if(!$settings)
+            throw new Exception('Error reading db-config');
+        $config = $settings['database'];
+        
+        $username = $config['username'];
+        $password = $config['password'];
+        $dns = $config['driver'] . ':host=' . $config['host'] . ';';
+        if (!empty($config['port']))
+            $dns .= 'port=' . $config['port'] . ';';
+        $dns .= 'dbname=' . $config['db_name'] . ';';
+        
+        $pdo = new PDO($dns, $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->exec('SET search_path to ' . $config['schema']);
+        return $pdo;
+    }
+}
--- sites/public/index.php
+++ sites/public/index.php
@@ -1,17 +1,11 @@
 <?php
-require_once('PDOLoader.php');
 require_once('Model.php');
-$loader = new PDOLoader('../db-config.ini');
-var_dump($loader);
-
-$q = 'SELECT * FROM test.artist';
-$loader->exec($q);
-
-$model = new Model();
-$model->setId(90);
-echo $model;
-
-// $dbc = pg_connect("host=postgresql dbname=postgres user=cedric password=frost-marker");
-// var_dump($dbc);
-// $db = new PDO('pgsql:host=postgresql;dbname=postgres;user=cedric;password=frost-marker');
-// var_dump($db);
+require_once('Connection.php');
+try {
+    $conn = Connection::init()->getConnection();
+    $statement = $conn->query('select * from customer');
+    $r = $statement->fetchAll();
+    var_dump($r);
+} catch (PDOException $e){
+    echo $e->getMessage();
+}
